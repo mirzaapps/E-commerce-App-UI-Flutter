76266f63db3e6df9073df15ea5b2212120de08e0
mirzaapps <85788342+mirzaapps@users.noreply.github.com>
2022-09-15 13:00:56 +0530

    Update Product.dart

--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -67,5 +67,5 @@ List<Product> products = [
   ),
 ];
 
-String dummyText =
+String dummyText = j
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. When an unknown printer took a galley.";
